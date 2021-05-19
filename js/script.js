de66525e58cb21e4ba70b824bc1ca2ab77fa468d
Kos Krzysztof <krzysztof.kos33@gmail.com>
2021-05-19 18:37:14 +0200

    Add menu hamburger for medium screen, add mixin breakpoint, update scss files

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 const navigation = document.querySelector('.navigation');
 
+const navigationMenu = document.querySelector('.navigation__menu');
+
 function scrolledNav() {
 
     let scrollY = window.scrollY;
 
     if(scrollY > 300) {
-
         navigation.classList.add('navigation--scrolled');
-
     } else {
-
         navigation.classList.remove('navigation--scrolled');
-
     }
-
 }
 
 window.addEventListener('scroll', scrolledNav);
+
+function toggleMenu() {
+
+    navigationMenu.classList.toggle('navigation__menu--active');
+
+}
+
+navigationMenu.addEventListener('click', toggleMenu);
